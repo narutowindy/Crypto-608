4323d281696aec1870367be661ad6df259149fba
narutowindy <narutowindy@users.noreply.github.com>
2018-10-25 13:00:55 -0400

    Add files via upload

--- sq_multliply.py
+++ sq_multliply.py
@@ -1,10 +1,20 @@
-import sys
-u = int(sys.arg[1])
-m = int(sys.arg[2])
-p = int(sys.arg[3])
+def pow_modm(x,e,m):
+    X = x
+    E = e
+    Y = 1
+    while E > 0:
+        if E % 2 == 0:
+            X = (X * X) % m
+            E = E/2
+        else:
+            Y = (X * Y) % m
+            E = E - 1
+    return Y
 
-print("A = u ^ m   mod(p) \n")
-print("binary rep of m",bin(m))
+print("Square and multiply alogorithm \t\n")
 
+b = int(input("Enter base value b:\t"))
+p = int(input("Enter power value p:\t"))
+m = int(input("Enter mod value m:\t"))
 
-
+print(pow_modm(b,p,m))
